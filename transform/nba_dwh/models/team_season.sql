749c9bdc18b65c9ba13aad08e7f0aec6f616f7d2
pdgarden <paul.desjardin1@gmail.com>
2024-04-20 20:23:44 +1000

    style(add-sql-linter): reformat models

--- transform/nba_dwh/models/team_season.sql
+++ transform/nba_dwh/models/team_season.sql
@@ -1,82 +1,66 @@
 {{
     config(
-        materialized='external',
-        location='output/team_season.csv',
-        tags=['gold'],
+        materialized="external",
+        location="output/team_season.csv",
+        tags=["gold"],
     )
 }}
 
 
-with game_summary as (
+with
+    game_summary as (select * from {{ ref("game_summary") }}),
 
-    select * from {{ ref('game_summary') }}
+    team as (select distinct home_team id from game_summary),
 
-),
+    game_summary_home as (
 
-team as (
+        select *, true is_home_team, home_team team_id
 
-    select distinct home_team id  from game_summary
+        from game_summary
 
-),
+        inner join team on team.id = game_summary.home_team
 
-game_summary_home as (
+    ),
 
-    select
-        *,
-        true is_home_team,
-        home_team team_id
+    game_summary_away as (
 
-    from game_summary
+        select *, false is_home_team, away_team team_id
 
-    inner join team on team.id = game_summary.home_team
+        from game_summary
+        inner join team on team.id = game_summary.away_team
 
-),
+    ),
 
-game_summary_away as (
+    game_summary_stack as (
+        select *
+        from game_summary_home
+        union all
+        select *
+        from game_summary_away
 
-    select
-        *,
-        false is_home_team,
-        away_team team_id
+    ),
 
-    from game_summary
-    inner join team on team.id = game_summary.away_team
+    final as (
 
-),
+        select
+            gss.team_id,
+            count(*) nb_game,
+            sum(
+                ((gss.is_home_team and gss.home_team_won) or (not gss.is_home_team and not gss.home_team_won))::int
+            ) nb_game_win,
+            avg(
+                ((gss.is_home_team and gss.home_team_won) or (not gss.is_home_team and not gss.home_team_won))::int
+            ) pct_game_win
 
-game_summary_stack as (
-    select * from game_summary_home
-    union all
-    select * from game_summary_away
+        from game_summary_stack gss
 
-),
+        where gss.is_regular_season
 
-final as (
+        group by gss.team_id
 
-    select
-        gss.team_id,
-        count(*) nb_game,
-        sum(
-            (
-                (gss.is_home_team and gss.home_team_won)
-                or
-                (not gss.is_home_team and not gss.home_team_won)
-            )::int) nb_game_win,
-        avg(
-            (
-                (gss.is_home_team and gss.home_team_won)
-                or
-                (not gss.is_home_team and not gss.home_team_won)
-            )::int) pct_game_win
+        order by gss.team_id
 
-    from game_summary_stack gss
+    )
 
-    where gss.is_regular_season
-
-    group by gss.team_id
-
-    order by gss.team_id
-
-)
-
-select * from final
+select *
+from final

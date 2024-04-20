b7ed992bbe5a991b9a722a072d5a248764279f8b
pdgarden <paul.desjardin1@gmail.com>
2024-04-20 20:23:44 +1000

    style(add-sql-linter): reformat models

--- transform/nba_dwh/models/game_summary.sql
+++ transform/nba_dwh/models/game_summary.sql
@@ -1,55 +1,44 @@
 {{
     config(
-        materialized='external',
-        location='output/game_summary.csv',
-        tags=['gold'],
+        materialized="external",
+        location="output/game_summary.csv",
+        tags=["gold"],
     )
 }}
 
 
-with game_boxscore as (
+with
+    game_boxscore as (select * from {{ ref("game_boxscore") }}),
 
-    select * from {{ ref('game_boxscore') }}
+    game_schedule as (select * from {{ source("local_source", "game_schedule_2024") }}),
 
-),
+    season_calendar as (select * from {{ source("local_source", "season_calendar") }}),
 
+    final as (
 
-game_schedule as (
+        select
+            gs.game_id game_id,
+            gs.date date,
+            gs.home_team home_team,
+            gs.away_team away_team,
+            sum(case when gb.team_name == gs.home_team then gb.points else 0 end) home_team_points,
+            sum(case when gb.team_name == gs.away_team then gb.points else 0 end) away_team_points,
+            (
+                sum(case when gb.team_name == gs.home_team then gb.points else 0 end)
+                > sum(case when gb.team_name == gs.away_team then gb.points else 0 end)
+            ) home_team_won,
+            gs.date between sc.start_date and sc.end_date is_regular_season
 
-    select * from {{ source('local_source', 'game_schedule_2024') }}
+        from game_schedule gs
 
-),
+        inner join game_boxscore gb on gb.game_id = gs.game_id
+        inner join season_calendar sc on sc.year = gs.season_year
 
-season_calendar as (
+        group by gs.game_id, gs.date, gs.home_team, gs.away_team, sc.start_date, sc.end_date
 
-    select * from {{ source('local_source', 'season_calendar') }}
+        order by gs.date, gs.home_team
+    )
 
-),
+select *
 
-final as (
-
-    select
-        gs.game_id game_id,
-        gs.date date,
-        gs.home_team home_team,
-        gs.away_team away_team,
-        sum(case when gb.team_name == gs.home_team then gb.points else 0 end) home_team_points,
-        sum(case when gb.team_name == gs.away_team then gb.points else 0 end) away_team_points,
-        (
-            sum(case when gb.team_name == gs.home_team then gb.points else 0 end)
-            >
-            sum(case when gb.team_name == gs.away_team then gb.points else 0 end)
-        ) home_team_won,
-        gs.date between sc.start_date and sc.end_date is_regular_season
-
-    from game_schedule gs
-
-    inner join game_boxscore gb on gb.game_id = gs.game_id
-    inner join season_calendar sc on sc.year = gs.season_year
-
-    group by gs.game_id, gs.date, gs.home_team, gs.away_team, sc.start_date, sc.end_date
-
-    order by gs.date, gs.home_team
-)
-
-select * from final
+from final

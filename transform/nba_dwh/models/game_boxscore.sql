71da6515ce0cd125f4eea6b94afeb1c71c9a1592
pdgarden <paul.desjardin1@gmail.com>
2024-04-20 20:19:17 +1000

    style(add-sql-linter): reformat game_boxscore model

--- transform/nba_dwh/models/game_boxscore.sql
+++ transform/nba_dwh/models/game_boxscore.sql
@@ -1,50 +1,49 @@
 {{
     config(
-        materialized='external',
-        location='output/game_boxscore.csv',
-        tags=['gold'],
+        materialized="external",
+        location="output/game_boxscore.csv",
+        tags=["gold"],
     )
 }}
 
 
-with game_boxscore as (
+with
+    game_boxscore as (select * from {{ source("local_source", "game_boxscore_2024") }}),
 
-    select * from {{ source('local_source', 'game_boxscore_2024') }}
+    final as (
 
-),
+        select
+            sha256(gb.game_id::varchar || gb.team::varchar || gb.starters) id,
+            gb.game_id::varchar game_id,
+            gb.team::varchar team_name,
+            gb.starters::varchar player_name,
+            (string_split(gb.mp, ':')[1]::float + (string_split(gb.mp, ':')[2]::float / 60))::float minute_played,
+            gb.fg::int field_goals_made,
+            gb.fga field_goals_attempts,
+            gb."FG%"::float field_goals_pct,
+            gb."3P"::int three_pts_made,
+            gb."3PA"::int three_pts_attempts,
+            gb."3P%"::float three_pts_pct,
+            gb.ft::int free_throw_made,
+            gb.fta::int free_throw_attempts,
+            gb."FT%"::float free_throw_pct,
+            gb.orb::int offensive_rebounds,
+            gb.drb::int defensive_rebounds,
+            gb.trb::int total_rebounds,
+            gb.ast::int assists,
+            gb.stl::int steals,
+            gb.blk::int blocks,
+            gb.tov::int turnovers,
+            gb.pf::int fouls,
+            gb.pts::int points,
+            gb."+/-"::int plus_minus
+        from game_boxscore gb
 
-final as (
+        where
+            gb.starters not in ('Starters', 'Reserves')  -- remove headers
+            and gb.mp like '%:%'  -- the player played the game
 
-    select
-        sha256(gb.game_id::varchar || gb.team::varchar || gb.Starters) id,
-        gb.game_id::VARCHAR game_id,
-        gb.team::VARCHAR team_name,
-        gb.Starters::VARCHAR player_name,
-        (string_split(gb.MP, ':')[1]::float + (string_split(gb.MP, ':')[2]::float / 60))::float minute_played,
-        gb.FG::int field_goals_made,
-        gb.FGA field_goals_attempts,
-        gb."FG%"::float field_goals_pct,
-        gb."3P"::int three_pts_made,
-        gb."3PA"::int three_pts_attempts,
-        gb."3P%"::float three_pts_pct,
-        gb.FT::int free_throw_made,
-        gb.FTA::int free_throw_attempts,
-        gb."FT%"::float free_throw_pct,
-        gb.ORB::int offensive_rebounds,
-        gb.DRB::int defensive_rebounds,
-        gb.TRB::int total_rebounds,
-        gb.AST::int assists,
-        gb.STL::int steals,
-        gb.BLK::int blocks,
-        gb.TOV::int turnovers,
-        gb.PF::int fouls,
-        gb.PTS::int points,
-        gb."+/-"::int plus_minus
-    from game_boxscore gb
+    )
 
-    where gb.Starters not in ('Starters', 'Reserves') -- remove headers
-        and gb.MP like '%:%' --the player played the game
-
-)
-
-select * from final
+select *
+from final

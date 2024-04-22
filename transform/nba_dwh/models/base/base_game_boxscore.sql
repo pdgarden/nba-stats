6217bbb587a713b29a2229bae714817bb61b75e5
pdgarden <paul.desjardin1@gmail.com>
2024-04-23 08:04:28 +1000

    feat(add-other-years): add model base_game_boxscore.sql

---
+++ transform/nba_dwh/models/base/base_game_boxscore.sql
@@ -0,0 +1,24 @@
+{{
+    config(
+        materialized="table",
+        tags=["bronze"],
+    )
+}}
+
+
+{% set season_years = [2023, 2024] %}
+
+
+with
+    final as (
+        {% for season_year in season_years %}
+            select *
+            from {{ source("local_source", "game_boxscore_" ~ season_year) }}
+            {% if not loop.last %}
+                union all
+            {% endif %}
+        {% endfor %}
+    )
+
+select *
+from final

fa108d65ebcd3a52b8bd686b5dacc934e5c3977b
pdgarden <paul.desjardin1@gmail.com>
2024-04-24 22:17:24 +1000

    feat(add-other-years): add years from 1999 and update modelization

--- transform/nba_dwh/models/game_summary.sql
+++ transform/nba_dwh/models/game_summary.sql
@@ -14,7 +14,7 @@ with
 
     season_calendar as (select * from {{ source("local_source", "season_calendar") }}),
 
-    final as (
+    summary as (
 
         select
             gs.game_id game_id,
@@ -23,10 +23,6 @@ with
             gs.away_team away_team,
             sum(case when gb.team_name == gs.home_team then gb.points else 0 end) home_team_points,
             sum(case when gb.team_name == gs.away_team then gb.points else 0 end) away_team_points,
-            (
-                sum(case when gb.team_name == gs.home_team then gb.points else 0 end)
-                > sum(case when gb.team_name == gs.away_team then gb.points else 0 end)
-            ) home_team_won,
             gs.date between sc.start_date and sc.end_date is_regular_season
 
         from game_schedule gs
@@ -36,7 +32,25 @@ with
 
         group by gs.game_id, gs.date, gs.home_team, gs.away_team, sc.start_date, sc.end_date
 
-        order by gs.date, gs.home_team
+    ),
+
+    final as (
+
+        select
+            s.game_id game_id,
+            s.date date,
+            s.home_team home_team,
+            s.away_team away_team,
+            s.home_team_points home_team_points,
+            s.away_team_points away_team_points,
+            s.is_regular_season is_regular_season,
+            case when s.home_team_points > s.away_team_points then s.home_team else s.away_team end winning_team,
+            case when s.home_team_points > s.away_team_points then s.away_team else s.home_team end losing_team
+
+        from summary s
+
+        order by s.date, s.home_team
+
     )
 
 select *

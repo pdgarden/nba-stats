f2ffc2fe7e585c8431104b31cb36bc4ca0bca82b
pdgarden <paul.desjardin1@gmail.com>
2024-07-30 20:10:05 +1000

    feat(improve-schema): add season table

---
+++ transform/nba_dwh/models/season.sql
@@ -0,0 +1,30 @@
+{{
+    config(
+        materialized="external",
+        location="output/season.csv",
+        tags=["gold"],
+    )
+}}
+
+
+with
+    season_calendar as (select * from {{ source("local_source", "season_calendar") }}),
+
+    final as (
+
+        select
+            sha256(sc.year::varchar) id,
+            concat(
+                date_part('year', sc.start_date::date)::varchar, '-', date_part('year', sc.end_date::date)::varchar
+            ) as years,
+            sc.start_date::date start_date,
+            sc.end_date::date end_date,
+            date_part('year', sc.start_date::date) start_year,
+            date_part('year', sc.end_date::date) end_year
+
+        from season_calendar sc
+
+    )
+
+select *
+from final

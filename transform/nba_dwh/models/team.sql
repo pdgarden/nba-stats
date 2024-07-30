6127bd7aeff39ef00ff58719c0824414bf638ff8
pdgarden <paul.desjardin1@gmail.com>
2024-07-30 20:48:23 +1000

    feat(improve-schema): add team table

---
+++ transform/nba_dwh/models/team.sql
@@ -0,0 +1,14 @@
+{{
+    config(
+        tags=["gold"],
+    )
+}}
+
+
+with
+    base_game_boxscore as (select * from {{ ref("base_game_boxscore") }}),
+
+    final as (select distinct sha256(bgb.team) id, bgb.team team_name from base_game_boxscore bgb order by bgb.team)
+
+select *
+from final

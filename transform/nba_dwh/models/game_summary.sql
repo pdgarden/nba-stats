40582cd4c5544e5c6558587c2393d67dec87a79b
pdgarden <paul.desjardin1@gmail.com>
2024-08-05 21:13:16 +1000

    feat(improve-schema): update game_summary by adding hash id

--- transform/nba_dwh/models/game_summary.sql
+++ transform/nba_dwh/models/game_summary.sql
@@ -17,39 +17,43 @@ with
     summary as (
 
         select
-            gs.game_id game_id,
+            sha256(gs.game_id) id,
+            gs.game_id game_name,
             gs.date date,
-            gs.home_team home_team,
-            gs.away_team away_team,
+            sha256(gs.home_team) home_team_id,
+            sha256(gs.away_team) away_team_id,
+            gs.date between sc.start_date and sc.end_date is_regular_season,
             sum(case when gb.team_name == gs.home_team then gb.points else 0 end) home_team_points,
-            sum(case when gb.team_name == gs.away_team then gb.points else 0 end) away_team_points,
-            gs.date between sc.start_date and sc.end_date is_regular_season
+            sum(case when gb.team_name == gs.away_team then gb.points else 0 end) away_team_points
 
         from game_schedule gs
 
         inner join game_boxscore gb on gb.game_id = gs.game_id
-        inner join season_calendar sc on sc.year = gs.season_year
+        inner join season_calendar sc on sha256(sc.year::varchar) = sha256(gs.season_year::varchar)
 
-        group by gs.game_id, gs.date, gs.home_team, gs.away_team, sc.start_date, sc.end_date
+        group by 1, 2, 3, 4, 5, 6
 
     ),
 
     final as (
 
         select
-            s.game_id game_id,
+            s.id id,
+            s.game_name game_name,
             s.date date,
-            s.home_team home_team,
-            s.away_team away_team,
+            s.home_team_id home_team_id,
+            s.away_team_id away_team_id,
             s.home_team_points home_team_points,
             s.away_team_points away_team_points,
             s.is_regular_season is_regular_season,
-            case when s.home_team_points > s.away_team_points then s.home_team else s.away_team end winning_team,
-            case when s.home_team_points > s.away_team_points then s.away_team else s.home_team end losing_team
+            case
+                when s.home_team_points > s.away_team_points then s.home_team_id else s.away_team_id
+            end winning_team_id,
+            case when s.home_team_points > s.away_team_points then s.away_team_id else s.home_team_id end losing_team_id
 
         from summary s
 
-        order by s.date, s.home_team
+        order by s.date, s.game_name
 
     )
 
